66bb642ebecaf918bc2225a146f229a9be93bc4a
makenova <themarchmyth@gmail.com>
2020-01-02 10:16:46 -0600

    add mute to macro pad

--- keyboards/pdxkbc/keymaps/makenova/keymap.c
+++ keyboards/pdxkbc/keymaps/makenova/keymap.c
@@ -2,12 +2,18 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
-			MO(1), MO(2), KC_VOLU, KC_F15, KC_VOLD, KC_F14
+			LT(1,KC_MUTE), MO(2),
+			KC_VOLU, KC_F15,
+			KC_VOLD, KC_F14
 		),
 	[1] = LAYOUT(
-			KC_TRNS, KC_R, KC_D, KC_X, KC_S, KC_Z
+			KC_TRNS, KC_R,
+			KC_D, KC_X,
+			KC_S, KC_Z
 		),
 	[2] = LAYOUT(
-			KC_SLEP, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO
+			KC_SLEP, KC_TRNS,
+			KC_NO, KC_NO,
+			RESET, KC_NO
 		)
 };
