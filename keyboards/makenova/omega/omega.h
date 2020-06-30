83b79fe7cc46fd64d8924fa01a903f36a87f8852
makenova <themarchmyth@gmail.com>
2020-06-30 11:42:39 -0500

    omega soldered blind update

--- keyboards/makenova/omega/omega.h
+++ keyboards/makenova/omega/omega.h
@@ -18,7 +18,8 @@
 
 #include "quantum.h"
 
-#define LAYOUT( \
+// hotswap 29 keys
+#define LAYOUT_hs_29( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, \
     K20, K21, K22, K23, K24, K26, K27, K28, K29 \
@@ -26,4 +27,26 @@
     { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09 }, \
     { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19 }, \
     { K20, K21, K22, K23, K24, KC_NO, K26, K27, K28, K29 } \
+}
+
+// soldered 29 keys
+#define LAYOUT_s_29( \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, \
+    K20, K21, K22, K23, K25, K26, K27, K28, K29 \
+){ \
+    { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09 }, \
+    { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19 }, \
+    { K20, K21, K22, K23, KC_NO, K25, K26, K27, K28, K29 } \
+}
+
+// soldered 30 keys
+#define LAYOUT_s_30( \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, \
+    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29 \
+){ \
+    { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09 }, \
+    { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19 }, \
+    { K20, K21, K22, K23, K24, K25, K26, K27, K28, K29 } \
 }
