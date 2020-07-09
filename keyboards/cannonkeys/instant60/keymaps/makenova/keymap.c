463e771c570ed62cc06fa6ae42147a7729ca52ff
makenova <themarchmyth@gmail.com>
2020-07-08 22:39:53 -0500

    foo

--- keyboards/cannonkeys/instant60/keymaps/makenova/keymap.c
+++ keyboards/cannonkeys/instant60/keymaps/makenova/keymap.c
@@ -18,11 +18,22 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 extern keymap_config_t keymap_config;
 
+enum {
+  TD_SEMI_QUOTE = 0,
+  TD_DOT_ENTER = 1
+};
+
+// Tap Dance Definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_SEMI_QUOTE]  = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, KC_QUOT),
+  [TD_DOT_ENTER]  = ACTION_TAP_DANCE_DOUBLE(KC_PDOT, KC_PENT)
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_60_tsangan_hhkb(
 		KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8,  KC_9, KC_0, KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
 		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
-		KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+		KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, TD(TD_SEMI_QUOTE), KC_QUOT, KC_ENT,
 		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT, MO(2),
 		KC_LCTL, KC_LALT, KC_LGUI, LT(1,KC_SPC), KC_RGUI, KC_RALT, KC_RCTL
   ),
