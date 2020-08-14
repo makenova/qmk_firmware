186fe8b87b1255b49e6f5b615e0d12accfd09715
makenova <themarchmyth@gmail.com>
2020-08-14 06:04:28 -0500

    update reverie layout
    remove space cadet shift
    remove tap actions from alphas

--- keyboards/makenova/reverie/config.h
+++ keyboards/makenova/reverie/config.h
@@ -68,7 +68,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
 
-#define TAPPING_TERM 200
+#define TAPPING_TERM 175
 
 #define LSPO_KEYS KC_LSFT, KC_LSFT, KC_LBRC
 #define RSPC_KEYS KC_RSFT, KC_RSFT, KC_RBRC
