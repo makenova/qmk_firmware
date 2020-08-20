1dbcd47b85510bf2b88b981a1a209410bc3e405a
makenova <themarchmyth@gmail.com>
2020-08-20 01:43:34 -0500

    update personal qaz keymap with symbols

--- keyboards/qaz/keymaps/makenova_big_space/keymap.c
+++ keyboards/qaz/keymaps/makenova_big_space/keymap.c
@@ -21,20 +21,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LAYOUT(
     KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_QUOT,
-    KC_TRNS, KC_LBRC, KC_LPRN, KC_UNDS, KC_RPRN, KC_RBRC, KC_RCBR, KC_SCLN, KC_TRNS,
+    KC_TRNS, KC_LBRC, KC_RBRC, KC_UNDS, KC_RPRN, KC_RBRC, KC_BSLS, KC_MINS, KC_EQL,
     KC_TRNS, KC_TRNS,      KC_TRNS,        KC_TRNS, KC_TRNS
   ),
   LAYOUT(
     KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
-    KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_MINS, KC_QUOT,
-    KC_LCBR, KC_LBRC, KC_LPRN, KC_UNDS, KC_RPRN, KC_RBRC, KC_RCBR, KC_SCLN, KC_QUOTE,
-    KC_TRNS, KC_TRNS,      KC_TRNS,        KC_TRNS, KC_TRNS
+    KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_MINS, KC_EQL,
+    KC_LCBR, KC_LBRC, KC_LPRN, KC_UNDS, KC_RPRN, KC_RBRC, KC_RCBR, KC_SCLN, KC_BSLS,
+    RESET, KC_TRNS,      KC_POWER,        KC_TRNS, KC_TRNS
   )
 };
 
 #ifdef COMBO_ENABLE
 const uint16_t PROGMEM combo_bspc[] = {KC_O, KC_P, COMBO_END};
 const uint16_t PROGMEM combo_numbak[] = {KC_0, KC_9, COMBO_END};
+const uint16_t PROGMEM combo_tilde[] = {KC_1, KC_2, COMBO_END};
 const uint16_t PROGMEM combo_tab[] = {KC_W, KC_E, COMBO_END};
 const uint16_t PROGMEM combo_esc[] = {KC_Q, KC_W, COMBO_END};
 const uint16_t PROGMEM combo_del[] = {KC_MINS, KC_EQL, COMBO_END};
@@ -47,5 +48,6 @@ combo_t key_combos[COMBO_COUNT] = {
   COMBO(combo_esc,KC_ESC), // 4
   COMBO(combo_del,KC_DEL), // 5
   COMBO(combo_ent,KC_ENT), // 6
+  COMBO(combo_tilde,KC_GRV), // 7
 };
 #endif
