ce1a984eed28bfbd1219433b2eaf7d97e465ac5c
makenova <themarchmyth@gmail.com>
2020-09-13 03:04:28 -0500

    update stupor keymap

--- keyboards/makenova/stupor/keymaps/default/config.h
+++ keyboards/makenova/stupor/keymaps/default/config.h
@@ -1,5 +1,5 @@
 
 #ifdef COMBO_ENABLE
-#    define COMBO_COUNT 6
+#    define COMBO_COUNT 5
 #    define COMBO_TERM 50
 #endif
--- keyboards/makenova/stupor/keymaps/default/keymap.c
+++ keyboards/makenova/stupor/keymaps/default/keymap.c
@@ -17,7 +17,6 @@
 
 #ifdef COMBO_ENABLE
 const uint16_t PROGMEM combo_bspc[] = {KC_J, KC_K, COMBO_END};
-const uint16_t PROGMEM combo_numbak[] = {KC_0, KC_9, COMBO_END};
 const uint16_t PROGMEM combo_tilde[] = {KC_1, KC_2, COMBO_END};
 const uint16_t PROGMEM combo_tab[] = {KC_W, KC_E, COMBO_END};
 const uint16_t PROGMEM combo_esc[] = {KC_Q, KC_W, COMBO_END};
@@ -25,11 +24,10 @@ const uint16_t PROGMEM combo_ent[] = {KC_G, KC_H, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
   COMBO(combo_bspc,KC_BSPC), // 1
-  COMBO(combo_numbak,KC_BSPC), // 2
-  COMBO(combo_tab,KC_TAB), // 3
-  COMBO(combo_esc,KC_ESC), // 4
-  COMBO(combo_ent,KC_ENT), // 5
-  COMBO(combo_tilde,KC_GRV), // 6
+  COMBO(combo_tab,KC_TAB), // 2
+  COMBO(combo_esc,KC_ESC), // 3
+  COMBO(combo_ent,KC_ENT), // 4
+  COMBO(combo_tilde,KC_GRV), // 5
 };
 #endif
 
@@ -37,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
     KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
     MT(MOD_LCTL,KC_TAB), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN,
-    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, LT(2,KC_SLSH),
+    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, RSFT_T(KC_DOT), LT(2,KC_SLSH),
     KC_LCTL, KC_LALT, KC_LGUI, LT(1,KC_SPACE), KC_RGUI, KC_RCTL
     ),
     [1] = LAYOUT(
