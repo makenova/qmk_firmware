f40979be10bf3ef25a4ace3a1d88310e8e6f2280
makenova <themarchmyth@gmail.com>
2020-10-12 01:56:19 -0500

    update omega4
    also fix boo boo in instant60 keymap

--- keyboards/primekb/prime_e/keymaps/makenova/keymap.c
+++ keyboards/primekb/prime_e/keymaps/makenova/keymap.c
@@ -23,10 +23,10 @@ combo_t key_combos[COMBO_COUNT] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LAYOUT(
-    KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T,                  KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL, KC_BSPC,
-    CTRL_TAB, KC_A, KC_S, KC_D, KC_F, KC_G,          KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
-    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,                 LT3_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-    KC_LCTL,  KC_LALT,  KC_LGUI, LT2_SPC,                  LT1_SPC, KC_RGUI,                   KC_RALT,  KC_RCTL
+    KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T,                          KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL, KC_BSPC,
+    CTRL_TAB, KC_A, KC_S, KC_D, KC_F, KC_G,                        KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
+    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,                         LT3_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+    KC_LCTL,  KC_LALT,  KC_LGUI, LT2_SPC,                          LT1_SPC, KC_RGUI,                   KC_RALT,  KC_RCTL
     ),
 
     LAYOUT(
