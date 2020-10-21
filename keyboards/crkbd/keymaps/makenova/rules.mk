c2545d98c2430d10216ca44916daef2c5b61e5ba
makenova <themarchmyth@gmail.com>
2020-10-20 21:21:15 -0500

    update oled corne code

--- keyboards/crkbd/keymaps/makenova/rules.mk
+++ keyboards/crkbd/keymaps/makenova/rules.mk
@@ -1,3 +1,7 @@
 COMBO_ENABLE = yes
 # TAP_DANCE_ENABLE = yes
 EXTRAKEY_ENABLE = yes       # Audio control and System control
+BOOTLOADER = dfu
+
+OLED_DRIVER_ENABLE = yes
+SRC +=  ./oled.c
--- keyboards/crkbd/keymaps/makenova5/config.h
+++ keyboards/crkbd/keymaps/makenova5/config.h
@@ -1,52 +1,20 @@
-/*
-This is the c configuration file for the keymap
-
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #pragma once
-
-//#define USE_MATRIX_I2C
-
-/* Select hand configuration */
 
 #define MASTER_LEFT
 // #define MASTER_RIGHT
-// #define EE_HANDS
-
-#define SSD1306OLED
 
 #define USE_SERIAL_PD2
 
+// #define OLED_FONT_H "keyboards/crkbd/keymaps/makenova/glcdfont.c"
+
 // #define TAPPING_FORCE_HOLD 
 #define TAPPING_TERM 150
-
-// #ifdef RGBLIGHT_ENABLE
-//     #undef RGBLED_NUM
-//     #define RGBLIGHT_ANIMATIONS
-//     #define RGBLED_NUM 27
-//     #define RGBLIGHT_LIMIT_VAL 120
-//     #define RGBLIGHT_HUE_STEP 10
-//     #define RGBLIGHT_SAT_STEP 17
-//     #define RGBLIGHT_VAL_STEP 17
-// #endif
-
+#define IGNORE_MOD_TAP_INTERRUPT
 
 #ifdef COMBO_ENABLE
 #    define COMBO_COUNT 4
 #    define COMBO_TERM 50
 #endif
+
+#define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 2000  /* Time (in ms) before the one shot key is released */
