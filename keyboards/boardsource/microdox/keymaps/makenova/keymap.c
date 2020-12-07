f4bfe81d359d7d689f9d425d3d9752fe461049fc
makenova <themarchmyth@gmail.com>
2020-12-07 00:34:06 -0600

    add gui to homerow on some keymaps

--- keyboards/boardsource/microdox/keymaps/makenova/keymap.c
+++ keyboards/boardsource/microdox/keymaps/makenova/keymap.c
@@ -26,6 +26,9 @@ combo_t key_combos[COMBO_COUNT] = {
 // alt
 #define ALT_S LALT_T(KC_S)
 #define ALT_L RALT_T(KC_L)
+//gui
+#define GUI_G LGUI_T(KC_G)
+#define GUI_H RGUI_T(KC_H)
 // shift
 #define SFT_Z LSFT_T(KC_Z)
 #define SFT_SLSH RSFT_T(KC_SLSH)
@@ -40,7 +43,7 @@ combo_t key_combos[COMBO_COUNT] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x5_3(
     KC_Q,    KC_W,   KC_E,     KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-    CTRL_A,  ALT_S,  KC_D,     KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    ALT_L,   CTRL_SCLN,
+    CTRL_A,  ALT_S,  KC_D,     KC_F,    GUI_G,       GUI_H,   KC_J,    KC_K,    ALT_L,   CTRL_SCLN,
     SFT_Z,   KC_X,   KC_C,     KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  SFT_SLSH,
                      KC_LALT,  KC_LGUI, LT2_ESC,        LT1_SPC, OS_SFT,  OS_3
   ),
