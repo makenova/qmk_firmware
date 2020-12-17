8f4f6af39081d268c6320f92c914bc7e52e7b541
makenova <themarchmyth@gmail.com>
2020-12-16 21:07:56 -0600

    add ferris keymap
    move all mods to homerow on microdox

--- keyboards/boardsource/microdox/keymaps/makenova/keymap.c
+++ keyboards/boardsource/microdox/keymaps/makenova/keymap.c
@@ -1,37 +1,33 @@
 #include QMK_KEYBOARD_H
 
 #ifdef COMBO_ENABLE
-const uint16_t PROGMEM combo_bspc[] = {KC_J, KC_K, COMBO_END};
-const uint16_t PROGMEM combo_numdel[] = {KC_0, KC_9, COMBO_END};
+const uint16_t PROGMEM combo_bspc[] = {KC_U, KC_I, COMBO_END};
+const uint16_t PROGMEM combo_del[] = {KC_M, KC_COMM, COMBO_END};
 const uint16_t PROGMEM combo_tilde[] = {KC_1, KC_2, COMBO_END};
-const uint16_t PROGMEM combo_tab[] = {KC_W, KC_E, COMBO_END};
-const uint16_t PROGMEM combo_esc[] = {KC_Q, KC_W, COMBO_END};
-const uint16_t PROGMEM combo_del[] = {KC_MINS, KC_EQL, COMBO_END};
-const uint16_t PROGMEM combo_ent[] = {KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM combo_ent[] = {KC_E, KC_R, COMBO_END};
+const uint16_t PROGMEM combo_tab[] = {KC_C, KC_V, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
   COMBO(combo_bspc,KC_BSPC), // 1
-  COMBO(combo_numdel,KC_DEL), // 2
-  COMBO(combo_tab,KC_TAB), // 3
-  COMBO(combo_esc,KC_ESC), // 4
-  COMBO(combo_del,KC_DEL), // 5
-  COMBO(combo_ent,KC_ENT), // 6
-  COMBO(combo_tilde,KC_GRV), // 7
+  COMBO(combo_del,KC_DEL), // 2
+  COMBO(combo_tilde,KC_GRV), // 3
+  COMBO(combo_ent,KC_ENT), // 4
+  COMBO(combo_tab,KC_TAB), // 5
 };
 #endif
 
 // ctrl
-#define CTRL_A LCTL_T(KC_A)
-#define CTRL_SCLN RCTL_T(KC_SCLN)
+#define CTRL_S LCTL_T(KC_S)
+#define CTRL_L RCTL_T(KC_L)
 // alt
-#define ALT_S LALT_T(KC_S)
-#define ALT_L RALT_T(KC_L)
+#define ALT_D LALT_T(KC_D)
+#define ALT_K RALT_T(KC_K)
 //gui
-#define GUI_G LGUI_T(KC_G)
-#define GUI_H RGUI_T(KC_H)
+#define GUI_F LGUI_T(KC_F)
+#define GUI_J RGUI_T(KC_J)
 // shift
-#define SFT_Z LSFT_T(KC_Z)
-#define SFT_SLSH RSFT_T(KC_SLSH)
+#define SFT_A LSFT_T(KC_A)
+#define SFT_SCLN RSFT_T(KC_SCLN)
 // layers
 #define OS_SFT OSM(MOD_RSFT)
 #define OS_3 OSL(3)
@@ -42,10 +38,10 @@ combo_t key_combos[COMBO_COUNT] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x5_3(
-    KC_Q,    KC_W,   KC_E,     KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-    CTRL_A,  ALT_S,  KC_D,     KC_F,    GUI_G,       GUI_H,   KC_J,    KC_K,    ALT_L,   CTRL_SCLN,
-    SFT_Z,   KC_X,   KC_C,     KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  SFT_SLSH,
-                     KC_LALT,  KC_LGUI, LT2_ESC,        LT1_SPC, OS_SFT,  OS_3
+    KC_Q,   KC_W,    KC_E,     KC_R,    KC_T,           KC_Y,    KC_U,   KC_I,    KC_O,   KC_P,
+    SFT_A,  CTRL_S,  ALT_D,    GUI_F,   KC_G,           KC_H,    GUI_J,  ALT_K,   CTRL_L, SFT_SCLN,
+    KC_Z,   KC_X,    KC_C,     KC_V,    KC_B,           KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH,
+                     KC_GRV,  OS_SFT, LT2_ESC,        LT1_SPC, OS_3, OS_SFT
   ),
 
   [1] = LAYOUT_split_3x5_3(
