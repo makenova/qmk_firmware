b7b847bf92a276fd821e54d2f7c919820f1d101f
makenova <themarchmyth@gmail.com>
2021-01-01 19:34:38 -0600

    put small keyboard keymap on instant60

--- keyboards/boardsource/microdox/keymaps/makenova/keymap.c
+++ keyboards/boardsource/microdox/keymaps/makenova/keymap.c
@@ -13,9 +13,8 @@
 #define SFT_A LSFT_T(KC_A)
 #define SFT_SCLN RSFT_T(KC_SCLN)
 // layers
-#define OS_SFT OSM(MOD_RSFT)
+#define OS_SFT OSM(MOD_LSFT)
 #define OS_3 OSL(3)
-#define OS_4 OSL(4)
 #define LT1_SPC LT(1,KC_SPC)
 #define LT2_ESC LT(2,KC_ESC)
 
@@ -38,34 +37,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,   KC_W,    KC_E,     KC_R,    KC_T,           KC_Y,    KC_U,   KC_I,    KC_O,   KC_P,
     SFT_A,  CTRL_S,  ALT_D,    GUI_F,   KC_G,           KC_H,    GUI_J,  ALT_K,   CTRL_L, SFT_SCLN,
     KC_Z,   KC_X,    KC_C,     KC_V,    KC_B,           KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH,
-                     KC_GRV,  OS_SFT, LT2_ESC,        LT1_SPC, OS_3, OS_SFT
+                     KC_LGUI,  OS_SFT, LT2_ESC,        LT1_SPC, OS_3, DF(4)
   ),
 
   [1] = LAYOUT_split_3x5_3(
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_QUOT,
-    KC_GRV,  KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS,      KC_TRNS, KC_BSLS, KC_MINS, KC_EQL,  KC_TRNS,
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,     KC_0,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_QUOT,
+    KC_GRV,  KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS,      KC_TRNS, KC_BSLS, KC_MINS, KC_EQL,   KC_TRNS,
                       KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
   [2] = LAYOUT_split_3x5_3(
-    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_DQUO,
-    KC_TILDE, KC_LCBR, KC_RCBR, KC_TRNS, KC_TRNS,      KC_TRNS, KC_PIPE, KC_UNDS, KC_PLUS,  KC_TRNS,
-                      KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS
+    KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
+    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_DQUO,
+    KC_TILDE, KC_LCBR, KC_RCBR, KC_TRNS, KC_TRNS,      KC_TRNS, KC_PIPE, KC_UNDS, KC_PLUS, KC_TRNS,
+                       KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
   [3] = LAYOUT_split_3x5_3(
-    KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,            KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
-    KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_TRNS,        KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_TRNS,
-    RESET, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_F14, KC_F15, KC_VOLD, KC_VOLU,  OS_4,
-             RGB_TOG, RGB_MODE_FORWARD, KC_SLEP,      KC_TRNS,  KC_TRNS, KC_TRNS
+    KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,          KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,
+    KC_F11, KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS,        KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_TRNS,
+    RESET,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_F14,  KC_F15,  KC_VOLD, KC_VOLU,  KC_TRNS,
+            RGB_TOG, RGB_MODE_FORWARD, KC_SLEP,        KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
   [4] = LAYOUT_split_3x5_3( \
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,\
-    KC_A,  KC_S,  KC_D,  KC_F,  KC_NO,                 KC_NO,  KC_J,  KC_K,  KC_L,  KC_SCLN,\
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,\
-                  KC_NO,   KC_NO,  KC_B,              KC_SPC,   KC_NO, KC_NO \
-  )
+    KC_A,  KC_S,  KC_D,  KC_F,  KC_NO,                KC_NO, KC_J,  KC_K,  KC_L,  KC_SCLN,\
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,\
+                  KC_NO, KC_NO, KC_B,                 KC_SPC, KC_NO, DF(0) \
+  ),
 };
--- keyboards/cannonkeys/instant60/keymaps/makenova/keymap.c
+++ keyboards/cannonkeys/instant60/keymaps/makenova/keymap.c
@@ -1,69 +1,76 @@
-/*
-Copyright 2012,2013 Jun Wako <wakojun@gmail.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 #include QMK_KEYBOARD_H
 
 extern keymap_config_t keymap_config;
 
+// ctrl
+#define CTRL_S LCTL_T(KC_S)
+#define CTRL_L RCTL_T(KC_L)
+// alt
+#define ALT_D LALT_T(KC_D)
+#define ALT_K RALT_T(KC_K)
+//gui
+#define GUI_F LGUI_T(KC_F)
+#define GUI_J RGUI_T(KC_J)
+// shift
+#define SFT_A LSFT_T(KC_A)
+#define SFT_SCLN RSFT_T(KC_SCLN)
+// layers
+#define OS_SFT OSM(MOD_LSFT)
+#define OS_GUI OSM(MOD_LGUI)
+#define OS_1 OSL(1)
+#define LT2_SPC LT(2,KC_SPC)
+#define LT3_ESC LT(3,KC_ESC)
+
 #ifdef COMBO_ENABLE
-const uint16_t PROGMEM combo_bspc[] = {KC_J, KC_K, COMBO_END};
-const uint16_t PROGMEM combo_tab[] = {KC_W, KC_E, COMBO_END};
 const uint16_t PROGMEM combo_esc[] = {KC_Q, KC_W, COMBO_END};
-const uint16_t PROGMEM combo_ent[] = {KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM combo_ent[] = {ALT_D, GUI_F, COMBO_END};
+const uint16_t PROGMEM combo_tab[] = {KC_C, KC_V, COMBO_END};
+const uint16_t PROGMEM combo_bspc[] = {GUI_J, ALT_K, COMBO_END};
+const uint16_t PROGMEM combo_del[] = {KC_M, KC_COMM, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
-  COMBO(combo_bspc,KC_BSPC), // 1
-  COMBO(combo_tab,KC_TAB), // 2
-  COMBO(combo_esc,KC_ESC), // 3
-  COMBO(combo_ent,KC_ENT), // 4
+  COMBO(combo_esc,KC_ESC),   // 1
+  COMBO(combo_ent,KC_ENT),   // 2
+  COMBO(combo_tab,KC_TAB),   // 3
+  COMBO(combo_bspc,KC_BSPC), // 4
+  COMBO(combo_del,KC_DEL),   // 5
 };
 #endif
 
-// layers
-#define LT1_SPC LT(1,KC_SPC)
-#define LT2_ESC LT(2,KC_ESC)
-
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_60_tsangan_hhkb(
-		KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8,  KC_9, KC_0, KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
-		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
-		KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT, LT2_ESC,
-		KC_LCTL, KC_LALT, KC_LGUI, LT1_SPC, KC_RGUI, KC_RALT, KC_RCTL
-  ),
-  [1] = LAYOUT_60_tsangan_hhkb(
-		KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_F14, KC_F15,
-		KC_CAPS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8,  KC_9, KC_0, KC_TRNS, KC_TRNS, KC_DEL,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_GRV, KC_MINS, KC_EQL, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-  ),
-	[2] = LAYOUT_60_tsangan_hhkb(
-		RGB_TOG, RGB_MOD, RGB_RMOD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		DF(3), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		BL_BRTG, BL_INC, BL_DEC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS, KC_TRNS,
-		RESET, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, DF(3), KC_TRNS
-	),
-	[3] = LAYOUT_60_tsangan_hhkb(
-		KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_BSPC,
-		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
-		KC_LCTL, KC_A, KC_S, KC_D,KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M,KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, DF(0),
-		KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_RGUI, KC_RALT, KC_RCTL
-	)
+    [0] = LAYOUT_60_tsangan_hhkb(
+        KC_ESC,  KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
+        KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
+        KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, LT3_ESC,
+        KC_LCTL, KC_LALT, KC_LGUI, LT2_SPC, KC_RGUI, KC_RALT, KC_RCTL
+    ),
+    [1] = LAYOUT_60_tsangan_hhkb(
+        KC_ESC,  KC_1,  KC_2,   KC_3,  KC_4,  KC_5, KC_6, KC_7,  KC_8,    KC_9,   KC_0,     KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
+        KC_TAB,  KC_Q,  KC_W,   KC_E,  KC_R,  KC_T, KC_Y, KC_U,  KC_I,    KC_O,   KC_P,     KC_LBRC, KC_RBRC, KC_BSPC,
+        MO(3), SFT_A, CTRL_S, ALT_D, GUI_F, KC_G, KC_H, GUI_J, ALT_K,   CTRL_L, SFT_SCLN, KC_QUOT, KC_ENT,
+        KC_LSFT, KC_Z,  KC_X,   KC_C,  KC_V,  KC_B, KC_N, KC_M,  KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT, LT3_ESC,
+        KC_LCTL, KC_LALT, KC_LGUI, LT2_SPC, KC_RGUI, KC_RALT, KC_RCTL
+    ),
+    [2] = LAYOUT_60_tsangan_hhkb(
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_F14, KC_F15,
+        KC_CAPS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS, KC_TRNS, KC_DEL,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS, KC_MINS, KC_EQL,  KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+    [3] = LAYOUT_60_tsangan_hhkb(
+        RGB_TOG, RGB_MOD, RGB_RMOD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        BL_BRTG, BL_INC,  BL_DEC,   KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS, KC_TRNS,
+        RESET, KC_TRNS, KC_TRNS, KC_TRNS, DF(0), DF(1), DF(4)
+    ),
+    [4] = LAYOUT_60_tsangan_hhkb(
+        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_BSPC,
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
+        KC_LCTL, KC_A, KC_S, KC_D,KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M,KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, DF(0),
+        KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_RGUI, KC_RALT, KC_RCTL
+    )
 };
