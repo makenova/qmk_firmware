6e26659cbceddcf3af35e2f121fd096948260ebc
makenova <themarchmyth@gmail.com>
2021-02-21 06:36:39 -0600

    add boards and update keymaps
    add micro_redox split and semi-split
    update keymaps with a couple vertical combos

--- keyboards/makenova/reverie/keymaps/default/keymap.c
+++ keyboards/makenova/reverie/keymaps/default/keymap.c
@@ -15,44 +15,56 @@
  */
 #include QMK_KEYBOARD_H
 
+// ctrl
+#define CTRL_S LCTL_T(KC_S)
+#define CTRL_L RCTL_T(KC_L)
+// alt
+#define ALT_D LALT_T(KC_D)
+#define ALT_K RALT_T(KC_K)
+//gui
+#define GUI_F LGUI_T(KC_F)
+#define GUI_J RGUI_T(KC_J)
+// shift
+#define SFT_A LSFT_T(KC_A)
+#define SFT_SCLN RSFT_T(KC_SCLN)
+// layers
+#define OS_SFT OSM(MOD_RSFT)
+#define OS_3 OSL(3)
+#define OS_4 OSL(4)
+#define LT1_SPC LT(1,KC_SPC)
+#define LT2_ESC LT(2,KC_ESC)
+
+#ifdef COMBO_ENABLE
+const uint16_t PROGMEM combo_bspc[] = {GUI_J, ALT_K, COMBO_END};
+const uint16_t PROGMEM combo_del[] = {KC_M, KC_COMM, COMBO_END};
+const uint16_t PROGMEM combo_ent[] = {GUI_F, ALT_D, COMBO_END};
+const uint16_t PROGMEM combo_tab[] = {KC_C, KC_V, COMBO_END};
+
+combo_t key_combos[COMBO_COUNT] = {
+  COMBO(combo_bspc,KC_BSPC), // 1
+  COMBO(combo_del,KC_DEL), // 2
+  COMBO(combo_ent,KC_ENT), // 3
+  COMBO(combo_tab,KC_TAB), // 4
+};
+#endif
+
 enum {
   TD_SEMI_QUOTE = 0,
-  TD_DOT_ENTER = 1,
-  TD_LSHIFT_CURLY = 2,
-  TD_RSHIFT_CURLY = 3
+  TD_DOT_ENTER = 1
 };
 
 // Tap Dance Definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_SEMI_QUOTE]  = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, KC_QUOT),
-  [TD_DOT_ENTER]  = ACTION_TAP_DANCE_DOUBLE(KC_PDOT, KC_PENT),
-  [TD_LSHIFT_CURLY]  = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_LBRC),
-  [TD_RSHIFT_CURLY]  = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_RBRC)
+  [TD_DOT_ENTER]  = ACTION_TAP_DANCE_DOUBLE(KC_PDOT, KC_PENT)
 };
-
-#ifdef COMBO_ENABLE
-const uint16_t PROGMEM combo_bspc[] = {KC_J, KC_K, COMBO_END};
-const uint16_t PROGMEM combo_numbak[] = {KC_0, KC_9, COMBO_END};
-const uint16_t PROGMEM combo_tilde[] = {KC_1, KC_2, COMBO_END};
-const uint16_t PROGMEM combo_tab[] = {KC_W, KC_E, COMBO_END};
-const uint16_t PROGMEM combo_esc[] = {KC_Q, KC_W, COMBO_END};
-const uint16_t PROGMEM combo_ent[] = {KC_D, KC_F, COMBO_END};
-
-combo_t key_combos[COMBO_COUNT] = {
-  COMBO(combo_bspc,KC_BSPC), // 1
-  COMBO(combo_tab,KC_TAB), // 2
-  COMBO(combo_esc,KC_ESC), // 3
-  COMBO(combo_ent,KC_ENT), // 4
-  COMBO(combo_tilde,KC_GRV), // 5
-};
-#endif
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LAYOUT(
     KC_P7, KC_P8, KC_P9, LCTL_T(KC_GRV), KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL, KC_BSPC,
-    KC_P4, KC_P5, KC_P6, LCTL_T(KC_TAB), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, TD(TD_SEMI_QUOTE), LT(3,KC_ENT),
-    KC_P1, KC_P2, KC_P3, KC_UP, TD(TD_LSHIFT_CURLY), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, TD(TD_RSHIFT_CURLY),
-    TD(TD_DOT_ENTER), LT(1, KC_P0), KC_LEFT, KC_DOWN, KC_RGHT, KC_LAPO, KC_LGUI, LT(2,KC_SPC), LT(1,KC_SPC), KC_RGUI, KC_RALT, KC_RCPC
+    KC_P4, KC_P5, KC_P6, LCTL_T(KC_TAB), SFT_A, CTRL_S, ALT_D, GUI_F, KC_G, KC_H, GUI_J, ALT_K, CTRL_L, SFT_SCLN, LT(3,KC_ENT),
+    KC_P1, KC_P2, KC_P3, KC_UP, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+    TD(TD_DOT_ENTER), LT(1, KC_P0), KC_LEFT, KC_DOWN, KC_RGHT, KC_LALT, KC_LGUI, LT(2,KC_SPC), LT(1,KC_SPC), KC_RGUI, KC_RALT, KC_RCTRL
     ),
     LAYOUT(
     KC_PSLS, KC_PAST, KC_PMNS, KC_TRNS, KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,  KC_EQL,
@@ -61,8 +73,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
     LAYOUT(
-    KC_PSLS, KC_PAST, KC_PMNS, KC_TRNS, KC_GRV, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS, KC_EQL,
-    KC_TRNS, KC_TRNS, KC_PPLS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_DQUO, KC_BSLS,
+    KC_PSLS, KC_PAST, KC_PMNS, KC_TRNS, KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS, KC_EQL,
+    KC_TRNS, KC_TRNS, KC_PPLS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_DQUO, KC_BSLS,
     KC_TRNS, KC_TRNS, KC_PENT, KC_TRNS, KC_TRNS, KC_LCBR, KC_RCBR, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PIPE, KC_UNDS, KC_PLUS, KC_BSLS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
