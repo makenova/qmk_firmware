005e88f56826b2b8f609f06e61745d838bf4cb0d
makenova <themarchmyth@gmail.com>
2021-02-28 04:20:54 -0600

    add appetizer board
    update readme for micro_redox

--- keyboards/makenova/appetizer/config.h
+++ keyboards/makenova/appetizer/config.h
@@ -20,32 +20,50 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x0000
-#define DEVICE_VER   0x0001
+#define VENDOR_ID 0xFEED
+#define PRODUCT_ID 0x0000
+#define DEVICE_VER 0x0001
 #define MANUFACTURER makenova
-#define PRODUCT      micro_redox_semi_split
+#define PRODUCT appetizer
+#define DESCRIPTION basic 65
 
 /* key matrix size */
-#define MATRIX_ROWS 4
-#define MATRIX_COLS 11
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 15
 
-#define MATRIX_ROW_PINS { D0, B7, B4, E6 }
-#define MATRIX_COL_PINS { D4, D5, D3, D2, D1, F0, F1, F4, F5,F6 }
+/*
+ * Keyboard Matrix Assignments
+ *
+ * Change this to how you wired your keyboard
+ * COLS: AVR pins used for columns, left to right
+ * ROWS: AVR pins used for rows, top to bottom
+ * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
+ *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
+ *
+ */
+#define MATRIX_ROW_PINS { F1, F0, E6, B7, F5 }
+#define MATRIX_COL_PINS { F4, D3, D2, D1, D0, F6, C7, C6, B6, B5, B4, D7, D6, D4, D5 }
+#define UNUSED_PINS
 
-// #define DIRECT_PINS {{ B6, C6, C7, B0, E6}}
-#define UNUSED_PINS {F7, B5}
-
-#define ENCODERS_PAD_A { D6 }
-#define ENCODERS_PAD_B { D7 }
-
-#define ENCODER_RESOLUTION 4
-
-/* COL2ROW, ROW2COL */
+/* COL2ROW, ROW2COL*/
 #define DIODE_DIRECTION COL2ROW
 
-#define SOFT_SERIAL_PIN D0
+#define RGB_DI_PIN B0
+#ifdef RGB_DI_PIN
+#    define RGBLIGHT_ANIMATIONS
+#    define RGBLED_NUM 20
+#    define RGBLIGHT_HUE_STEP 8
+#    define RGBLIGHT_SAT_STEP 8
+#    define RGBLIGHT_VAL_STEP 8
+#endif
 
+
+/*
+ * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
+ */
+// #define SOFT_SERIAL_PIN D0  // or D1, D2, D3, E6
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
 
 /* define if matrix has ghost (lacks anti-ghosting diodes) */
@@ -55,10 +73,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
+
 /* disable these deprecated features by default */
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
-
-/* Bootmagic Lite key configuration */
-//#define BOOTMAGIC_LITE_ROW 0
-//#define BOOTMAGIC_LITE_COLUMN 0
--- keyboards/makenova/micro_redox/semi_split/keymaps/default/config.h
+++ keyboards/makenova/micro_redox/semi_split/keymaps/default/config.h
@@ -10,3 +10,5 @@
 
 #define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped once again. */
 #define ONESHOT_TIMEOUT 2000  /* Time (in ms) before the one shot key is released */
+
+#define ENCODER_DIRECTION_FLIP // flip encoder rotation direction
